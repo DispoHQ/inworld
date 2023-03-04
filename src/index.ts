28e1704d061e4a6c094a30354cde32db5b252c2a
justinmann <justin@dispo.fun>
2023-03-04 10:40:49 -0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,89 @@
 import { InworldClient, InworldPacket } from "@inworld/nodejs-sdk";
+import { question } from "cli-interact";
 
-async function sayHello() {
-  const client = new InworldClient()
-    // Get key and secret from the integrations page.
-    .setApiKey({
-      key: "KEY",
-      secret: "SECRET",
-    })
-    // Setup a user name.
-    // It allows character to call you by name.
-    .setUser({ fullName: "Your name" })
-    // Setup required capabilities.
-    // In this case you can receive character emotions.
-    .setConfiguration({
-      capabilities: { emotions: true },
-    })
-    // Use a full character name.
-    // It should be like workspaces/{WORKSPACE_NAME}/characters/{CHARACTER_NAME}.
-    // Or like workspaces/{WORKSPACE_NAME}/scenes/{SCENE_NAME}.
-    .setScene("workspaces/Dispo/characters/Juan")
-    // Attach handlers
-    .setOnError((err: Error) => console.error(err))
-    .setOnMessage((msg: InworldPacket) => {
-      console.log(msg);
+const leftClient = new InworldClient()
+  .setApiKey({
+    key: "6xxAAiwpi7KaewqNY3nEUib9BdWHKjtx",
+    secret: "23u7F9wlDMT3ilEv26KxntXXpdrzwdCZcOC5YaJB9DxqaSMwgpY2NyPazoH0RafJ",
+  })
+  .setUser({ fullName: "JUDY" })
+  .setClient({ id: "JUDY" })
+  .setConfiguration({
+    connection: { autoReconnect: false },
+    capabilities: { emotions: true },
+  })
+  .setScene("workspaces/dispo/characters/judy")
+  .setOnError((err: Error) => console.error(err))
+  .setOnMessage(async (msg: InworldPacket) => {
+    if (msg.text && msg.text.final) {
+      console.log("JUDY:", msg.text.text);
+      // console.log("FFF", rightBot);
+      setTimeout(() => {
+        sendRight(msg.text.text);
+      });
+    }
+    if (msg.isInteractionEnd()) {
+      console.log("JUDY: [Interaction Over]");
+      // leftBot.close();
+    }
+  });
+const leftBot = leftClient.build();
 
-      // Close connection.
-      connection.close();
-    });
+const rightClient = new InworldClient()
+  // Get key and secret from the integrations page.
+  .setApiKey({
+    key: "6xxAAiwpi7KaewqNY3nEUib9BdWHKjtx",
+    secret: "23u7F9wlDMT3ilEv26KxntXXpdrzwdCZcOC5YaJB9DxqaSMwgpY2NyPazoH0RafJ",
+  })
+  .setUser({ fullName: "JUAN" })
+  .setClient({ id: "JUAN" })
+  .setConfiguration({
+    connection: { autoReconnect: false },
+    capabilities: { emotions: true },
+  })
+  .setScene("workspaces/dispo/characters/juan")
+  .setOnError((err: Error) => console.error(err))
+  .setOnMessage(async (msg: InworldPacket) => {
+    if (msg.text && msg.text.final) {
+      console.log("JUAN:", msg.text.text);
+      setTimeout(() => {
+        sendLeft(msg.text.text);
+      }, 1000);
+    }
+    if (msg.isInteractionEnd()) {
+      console.log("JUAN: [Interaction Over]");
+      // rightBot.close();
+    }
+  });
+const rightBot = rightClient.build();
 
-  console.log("A1");
-
-  // Finish connection configuration.
-  const connection = client.build();
-
-  console.log("A2");
-
-  // Send your message to a character.
-  await connection.sendText("Hello");
+async function sendLeft(text: string) {
+  // console.log(leftBot);
+  try {
+    if (!leftBot.isActive()) {
+      await leftBot.open();
+    }
+  } catch {}
+  await leftBot.sendText(text);
 }
 
-sayHello();
+async function sendRight(text: string) {
+  try {
+    if (!rightBot.isActive()) {
+      await rightBot.open();
+    }
+  } catch {}
+  await rightBot.sendText(text);
+}
+
+let text = question("?");
+sendRight(text);
+
+// setTimeout(() => {
+//   console.log("DONE");
+//   // sendRight("Jack");
+//   // sendLeft("What is up?");
+//   // setTimeout(() => {
+//   //   console.log("DONE2");
+//   // }, 1000);
+// }, 1000);
